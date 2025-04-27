857569455333a89937eadda9d994885d0a4bbdad
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-04-27 10:39:45 +0300

    feat: add user component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
+import { UserComponent } from './components/user/user.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
